2033218470a39dd4b2cad4073d0d038da23db128
DESKTOP-D05M3O5\Optiplex <jarvis.avinash@gmail.com>
2023-08-22 19:06:11 +0530

    Add project files.

---
+++ Backend/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Backend.Data;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginUser(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == login.Email && u.Password == login.Password);
+
+                if (user != null)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return NotFound(); // User not found or invalid credentials
+                }
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ Backend/Program.cs
@@ -0,0 +1,36 @@
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddCors();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(p => p.AddPolicy("corspolicy", build =>
+{
+    build.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+}));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("corspolicy");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
